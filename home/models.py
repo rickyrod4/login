0fa2e81ee2f24be70068743d40c9eb7163f68d12
Ricky Rodriguez <ricardo.rodriguez4@yahoo.com>
2020-07-22 00:00:12 -0500

    first commit

---
+++ home/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+import bcrypt
+import re
+
+# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    password = models.CharField(max_length=255)
